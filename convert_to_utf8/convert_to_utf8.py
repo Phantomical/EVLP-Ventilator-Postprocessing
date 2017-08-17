e66ed1e8e919763047a89ff90534758568771772
Phantomical <phantom@lynches.ca>
2017-08-17 13:39:26 -0400

    Added file to attempt to convert to utf-8 from UCS-2

---
+++ convert_to_utf8/convert_to_utf8.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+
+import sys
+import codecs
+
+def convert_file(inputfile, outputfile, encoding):
+    with codecs.open(inputfile, 'rb', encoding) as source:
+        with codecs.open(outputfile, 'wb', 'utf-8') as target:
+            while True:
+                contents = source.readline()
+                if not contents:
+                    break
+                target.write(contents)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('usage: convert_to_utf8.py <input_file> <output_file>')
+        sys.exit(-1)
+    convert_file(sys.argv[1], sys.argv[2], 'utf-8')
